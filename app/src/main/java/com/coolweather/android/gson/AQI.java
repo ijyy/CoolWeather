1711ef495e4631d03d9d0a215538767043b6bf3e
Your Name <you@example.com>
2018-12-18 11:35:16 +0800

    First commit.

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by lenovo on 2018/11/28.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
